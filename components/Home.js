8f2cb62566c052b147be2846567283fe0cfabc5c
Wali <walisylla.esse@gmail.com>
2025-02-18 15:56:35 +0100

    mobile first home

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,34 @@
 import styles from '../styles/Home.module.css';
+import Button from './Button';
+import { useRouter } from 'next/router';
+
+
 
 function Home() {
-  
+  console.log('client side');
+  const router = useRouter();//Permet le changement de page
+  const handleClick = () => {
+    console.log('Clic');
+    router.push('/presentation');
+    
+  }
+
   return (
-    <div>
+    <>
+    {/* <div className={styles.wallpaper}><h2>Filtre</h2></div> */}
       <main className={styles.main}>
+        <p className={styles.name}>Wali Sylla</p>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          Developpeur Web Full Stack
+        </h1>      
+      <Button
+        onClick={handleClick}//Au clic sur le bouton, on est redirigé vers la page /presentation
+        text="En savoir plus" 
+      />
+
       </main>
-    </div>
+    
+    </>
   );
 }
 
---
+++ components/Presentation.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Presentation.module.css';
+
+function Presentation() {
+  return (
+    <div className={styles.wallpaper}>
+      <h1>Wali Sylla</h1>
+    </div>
+  );
+}
+
+export default Presentation;
