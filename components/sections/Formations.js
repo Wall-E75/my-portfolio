0bec58e5c2bf17accd83653c8d17fe6c01a6c4ab
Wali <walisylla.esse@gmail.com>
2025-05-24 03:25:39 +0200

    maj du composant formation

--- components/sections/Formations.js
+++ components/sections/Formations.js
@@ -1,33 +1,39 @@
 import styles from '@styles/Formations.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import { merriweather, raleway } from '../ui/fonts';
 
 function Formations() {
     return(
         <>
-            <main className={styles.main}>
-                <h1 className={styles.title}>Formations</h1>
+            <main className={`${styles.main} ${raleway.className}`}>
+                <h1 className={`${styles.title} ${raleway.className}`}>
+                    Formations & Certifications
+                </h1>
                 <div className={styles.iconContainer}>
-                        <FontAwesomeIcon className={styles.icon} color='#d8d8d8' icon={faGraduationCap} />
+                        <FontAwesomeIcon 
+                            className={styles.icon} 
+                            icon={faGraduationCap} 
+                        />
                 </div>
                 <div className={styles.formationContainer}>
                     <div className={styles.infos}>
-                        <h2>Diplome</h2>
-                        <p>2025 : Titre RNCP niveau 6 (Bac+ 3/4): Concepteur Developpeur d'application Web et Mobile</p>
+                        <h2 className={merriweather.className}>Diplôme</h2>
+                        <p><strong>2025</strong> : Titre RNCP niveau 6 (Bac+ 3/4)</p>
+                        <p>Concepteur Développeur d'Application Web et Mobile</p>
                     </div>
                     <div className={styles.infos}>
-                        <h2>Formations</h2>
-                        <p>2024 - 2025 : CodingBootCamp - La Capsule</p>
-                        <p>2022 - 2023 : Developpeur web - OpenClassRoom</p>
+                        <h2 className={merriweather.className}>Formations</h2>
+                        <p><strong>2024 - 2025</strong> : CodingBootCamp - La Capsule</p>
+                        <p><strong>2022 - 2023</strong> : Developpeur web - OpenClassRoom</p>
                     </div>   
                     <div className={styles.infos}>
-                        <h2>Langues</h2>
-                        <p>Français : langue maternelle</p>
-                        <p>Anglais : compréhension écrite avancée, expression orale intermédiaire</p>
-                        <p>Soninké : compréhension oral avancé, expression orale intermédiaire</p>
-                        <p>Arabe : compréhension orale basique, lu et écrit</p>
+                        <h2 className={merriweather.className}>Langues</h2>
+                        <p><strong>Français</strong> : langue maternelle</p>
+                        <p><strong>Anglais</strong> : compréhension écrite avancée, expression orale intermédiaire</p>
+                        <p><strong>Soninké</strong> : compréhension oral avancé, expression orale intermédiaire</p>
+                        <p><strong>Arabe</strong> : compréhension orale basique, lu et écrit</p>
                     </div>
-
                 </div>
             </main>
         </>
