2e2443c8af18eb84030368f2c1852c2ab8697d74
Wali <walisylla.esse@gmail.com>
2025-05-25 23:22:54 +0200

    header à finaliser

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -5,51 +5,53 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 import Menu from '@components/layouts/Menu';
 import { useVisibility } from "../../context/VisibilityContext";
 import ThemeSwitcher from '@components/ui/ThemeSwitcher';
-
+import { raleway } from '../ui/fonts'; 
 
 function Header() {
   const { displayComponant } = useVisibility();
   return (
     <>
-      <header className={styles.header}>
-       
+      <header className={`${styles.header} ${raleway.className || ''}`}>
         <div className={styles.logo}>
-          <Link href="/#presentation"><FontAwesomeIcon icon={faUser} className={styles.icon} /></Link>
+          <Link href="/#presentation" aria-label='Retour à l'accueil>
+            <FontAwesomeIcon icon={faUser} className={styles.icon} />
+          </Link>
           <ThemeSwitcher />
         </div>
         <div className={styles.menu}>
-          {/* <ThemeSwitcher className={styles.icon} /> */}
           <Menu className={styles.icon} />
         </div>
-        <nav className={styles.nav}>
-      
+
+        <nav className={styles.nav} aria-label='Navigation principal'>
             <ul className={styles.list}>
-                <li className={styles.project} title="projets">
+                <li className={styles.project} title="Découvrez mes projets">
                   <div className={styles.wallPaper}>
                     <Link href="/#projets">Projets</Link>
-
                   </div>
                 </li>
-                <li className={styles.competences}>
+
+                <li className={styles.competences} title='Mes compétences techniques'>
                   <div className={styles.wallPaper}>
                     <Link href="/#competences">Compétences</Link>
-
                   </div>
                 </li>
-                {displayComponant && <li className={styles.experiences}>
-                  <div className={styles.wallPaper}>
 
-                    <Link href='/#experiences'>Expériences</Link>
-                  </div>
-                </li>}
-                <li className={styles.formations}>
+                {displayComponant && (
+                  <li className={styles.experiences}>
+                    <div className={styles.wallPaper}>
+                      <Link href='/#experiences'>Expériences</Link>
+                    </div>
+                  </li>
+                )}
+
+                <li className={styles.formations} title='Ma formation et mes certifications'>
                   <div className={styles.wallPaper}>
                     <Link href='/#formations'>Formations</Link>
                   </div>
                 </li>
-                <li className={styles.contact}>
-                  <div className={styles.wallPaper}>
 
+                <li className={styles.contact} title='Contactez-moi'>
+                  <div className={styles.wallPaper}>
                     <Link href='/#contact'>Contact</Link>
                   </div>
                 </li>
