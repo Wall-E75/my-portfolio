714516d36e5680475917d53787a0874aa9ce431c
Wali <walisylla.esse@gmail.com>
2026-03-10 03:03:02 +0100

    hore: remove dead files (.babelrc.backup, Presentation.txt, TechnoProject.module.css)

--- updateStructure.js
+++ updateStructure.js
@@ -2,16 +2,31 @@
 import fs from 'fs';
 import path from 'path';
 
-const rootDir = '.'; // dossier racine du projet
+const rootDir = '.';
+
+const EXCLUDED_DIRS = new Set([
+  'node_modules', '.next', '.git', '.turbo', 'coverage', 'dist', 'build',
+]);
+
+const EXCLUDED_FILES = new Set([
+  'structure.txt', '.DS_Store', 'Thumbs.db',
+]);
+
+const EXCLUDED_EXTENSIONS = new Set([
+  '.lock', '.log',
+]);
 
 function readDirRecursive(dir, prefix = '') {
   let output = '';
   const items = fs.readdirSync(dir, { withFileTypes: true });
   for (const item of items) {
     if (item.isDirectory()) {
+      if (EXCLUDED_DIRS.has(item.name)) continue;
       output += `\n${prefix}${item.name}/:\n`;
       output += readDirRecursive(path.join(dir, item.name), prefix + '  ');
     } else {
+      if (EXCLUDED_FILES.has(item.name)) continue;
+      if (EXCLUDED_EXTENSIONS.has(path.extname(item.name))) continue;
       output += `${prefix}${item.name}\n`;
     }
   }
